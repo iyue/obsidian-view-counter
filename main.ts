ba0f87552ffad0ae81ab551e12adf3652e70bcae
Chunpeng Yue <chunpeng.iyue@gmail.com>
2023-05-05 23:36:55 +0800

    first commit

--- main.ts
+++ main.ts
@@ -1,81 +1,95 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {App, Modal, Plugin, PluginSettingTab, Setting} from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+	// mySetting: string;
+	viewCountKey: string;
+	lastViewDateKey: string;
+	interval: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	// mySetting: '',
+	viewCountKey: "view_count",
+	lastViewDateKey: "last_view_date",
+	interval: '120',
+}
+
+function log(e: any) {
+	if ((window as any)._debug) {
+		console.log(e);
+	}
+}
+
+function formatDate(date: Date) {
+	return date.getFullYear() +
+		"-" +
+		String(date.getMonth() + 1).padStart(2, '0') +
+		"-" +
+		String(date.getDate()).padStart(2, '0') +
+		" " +
+		String(date.getHours()).padStart(2, '0') +
+		":" +
+		String(date.getMinutes()).padStart(2, '0') +
+		":" +
+		String(date.getSeconds()).padStart(2, '0');
 }
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
+		const _this = this;
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		const app = this.app;
+		app.workspace.on('file-open', (file) => {
+			log(file);
+			if(file?.extension !== 'md') {
+				return;
+			}
+			let isExclude = false;
+
+			// // 过滤文件
+			// this.settings.mySetting.split(',').forEach(folder => {
+			// 	if(folder.trim() !== '' && file?.path.startsWith(folder.trim())) {
+			// 		isExclude = true;
+			// 		return;
+			// 	}
+			// })
+			// if(isExclude) {
+			// 	return;
+			// }
+			app.fileManager.processFrontMatter(file, (metadata) => {
+				log(metadata);
+				const viewCountKey = _this.settings.viewCountKey;
+				const viewCount = metadata[viewCountKey];
+				if (!viewCount) {
+					metadata[viewCountKey] = 1;
+				}
+				const lastViewTimeKey = _this.settings.lastViewDateKey;
+				const lastViewTime = metadata[lastViewTimeKey];
+				if (!lastViewTime) {
+					metadata[lastViewTimeKey] = formatDate(new Date());
+				}
+				const lastViewDate = new Date(metadata[lastViewTimeKey]);
+				const timeDiff = (new Date().getTime() - lastViewDate.getTime()) / (1000 * 60);
+				// 如果间隔大于预设, 则更新数据
+				if (timeDiff >= Number(_this.settings.interval)) {
+					metadata[viewCountKey] += 1;
+					metadata[lastViewTimeKey] = formatDate(new Date());
+				}
+			})
+		})
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+		statusBarItemEl.setText('ViewCounter');
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -97,12 +111,12 @@ class SampleModal extends Modal {
 	}
 
 	onOpen() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.setText('Woah!');
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
@@ -116,21 +130,55 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', { text: 'Settings for ViewCounter.' });
+
+		// new Setting(containerEl)
+		// 	.setName('files to exclude')
+		// 	.setDesc('you do not want log')
+		// 	.addText(text => text
+		// 		.setPlaceholder('e.g. DailyNotes, Readwise/Articles')
+		// 		.setValue(this.plugin.settings.mySetting)
+		// 		.onChange(async (value) => {
+		// 			log('Secret: ' + value);
+		// 			this.plugin.settings.mySetting = value;
+		// 			await this.plugin.saveSettings();
+		// 		}));
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('interval')
+			.setDesc('时间间隔')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('e.g. interval')
+				.setValue(this.plugin.settings.interval)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					log('Secret: ' + value);
+					this.plugin.settings.interval = value || DEFAULT_SETTINGS.interval;
+					await this.plugin.saveSettings();
+				}));
+		new Setting(containerEl)
+			.setName('viewCount name')
+			.setDesc('custom log key name')
+			.addText(text => text
+				.setPlaceholder('e.g. view_count')
+				.setValue(this.plugin.settings.viewCountKey)
+				.onChange(async (value) => {
+					log('Secret: ' + value);
+					this.plugin.settings.viewCountKey = value || DEFAULT_SETTINGS.viewCountKey;
+					await this.plugin.saveSettings();
+				}));
+		new Setting(containerEl)
+			.setName('lastViewDate name')
+			.setDesc('custom log key name')
+			.addText(text => text
+				.setPlaceholder('e.g. last_view_date')
+				.setValue(this.plugin.settings.lastViewDateKey)
+				.onChange(async (value) => {
+					log('Secret: ' + value);
+					this.plugin.settings.lastViewDateKey = value || DEFAULT_SETTINGS.lastViewDateKey;
 					await this.plugin.saveSettings();
 				}));
 	}
